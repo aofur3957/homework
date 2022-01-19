01604bc95f059a031d1992e6893ecf5c9481b8ce
aofur3957 <sunggin69@gmail.com>
2022-01-20 03:50:32 +0900

    스와이퍼 추가

---
+++ js/swiper.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: "auto",
+    centeredSlides: true,
+    spaceBetween: 30,
+    loop: true,
+    speed: 700,
+    
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+
+});
