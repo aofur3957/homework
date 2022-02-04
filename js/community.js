42a871a2e978cee4e886f035c403b850d148a59d
aofur3957 <sunggin69@gmail.com>
2022-02-04 23:19:57 +0900

    tab, scroll script

--- js/community.js
+++ js/community.js
@@ -1,14 +1,34 @@
 const tab = document.querySelector('#tab');
-const tabTites = tab.querySelectorAll('dt');
+const tabTitles = tab.querySelectorAll('dt');
+const tabContents = tab.querySelectorAll('dd');
 const faqOffset = document.querySelector('.faqHeaderSection').offsetTop;
-let scroll = window.scrollY || window.pageYOffset;
+const base = -300;
 
-if(scroll >= faqOffset){
-    
-}
 
-tabTites.forEach((tabTitle, idx)=>{
-    tabTitle.addEventListener(e=>{
-        
+window.addEventListener('scroll', e=>{
+    let scroll = window.scrollY || window.pageYOffset;
+
+    if(scroll >= faqOffset + base){
+        tabTitles.forEach((tabTitle, idx)=>{
+            let faceLine = tabTitle.querySelector('.cls-2');
+            let mouthLine = tabTitle.querySelector('.cls-3');
+            
+            faceLine.style.animationPlayState = 'running';
+            mouthLine.style.animationPlayState = 'running';
+        })
+    }
+})
+
+
+tabTitles.forEach((tabTitle, idx)=>{
+    tabTitle.addEventListener('click', e=>{
+        for(let el of tabTitles){
+            el.classList.remove('on');
+        }
+        for(let el of tabContents){
+            el.classList.remove('on');
+        }
+        tabTitles[idx].classList.add('on');
+        tabContents[idx].classList.add('on');
     })
 })
