14de4a2260bed07b79e9e776e98dd503379c8e21
aofur3957 <sunggin69@gmail.com>
2022-02-04 20:51:49 +0900

    sample svg

---
+++ js/community.js
@@ -0,0 +1,8 @@
+const tab = document.querySelector('#tab');
+const tabTites = tab.querySelectorAll('dt');
+
+tabTites.forEach((tabTitle, idx)=>{
+    tabTitle.addEventListener(e=>{
+        
+    })
+})
