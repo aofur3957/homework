be16dc85ecedd8ae0c6475500903800a4674f054
aofur3957 <sunggin69@gmail.com>
2022-02-04 22:46:39 +0900

    svg animation

--- js/community.js
+++ js/community.js
@@ -1,5 +1,11 @@
 const tab = document.querySelector('#tab');
 const tabTites = tab.querySelectorAll('dt');
+const faqOffset = document.querySelector('.faqHeaderSection').offsetTop;
+let scroll = window.scrollY || window.pageYOffset;
+
+if(scroll >= faqOffset){
+    
+}
 
 tabTites.forEach((tabTitle, idx)=>{
     tabTitle.addEventListener(e=>{
