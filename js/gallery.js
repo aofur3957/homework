a6b60f555d08a99615018e42ac4bca7b8c38d6f4
aofur3957 <sunggin69@gmail.com>
2022-02-06 19:47:16 +0900

    gallery loading

--- js/gallery.js
+++ js/gallery.js
@@ -8,16 +8,20 @@ method: flickr.interestingness.getList
 
 https://live.staticflickr.com/{server-id}/{id}_{secret}_{size-suffix}.jpg
 */
+const container = document.querySelector('#gallery')
 const gallery = document.querySelector('.gallery');
+const loading = document.querySelector('.loading');
 
 const base = 'https://www.flickr.com/services/rest/?';
 const method = 'flickr.interestingness.getList';
+const method2 = 'flickr.photos.search';
 const api_key = '6695bb82cf9a3db1962df3f386dd83e8';
 const per_page = 10;
 const format = 'json';
 
 let url1 = `${base}method=${method}&api_key=${api_key}&per_page=${per_page}&format=${format}&nojsoncallback=1`;
 
+let url2 = ``
 
 fetch(url1)
 .then(data=>{
@@ -25,7 +29,6 @@ fetch(url1)
 })
 .then(json=>{
     let items = json.photos.photo;
-    console.log(items);
 
     let htmls = '';
     items.map(item=>{
@@ -51,18 +54,35 @@ fetch(url1)
                         </a>
                         <a href="${imgBigSrc}">view more</a>
                     </article>
-                        `
+                        `;
+
     })
     gallery.innerHTML = htmls;
+
+    const imgs = gallery.querySelectorAll('img');
+    const len = imgs.length;
+    let count = 0;
+
+    for(let img of imgs){
+        img.onload = function(){
+            count++;
+            if(len === count){
+                loading.classList.add('off');
+                gallery.classList.add('on');
+            }
+        }
+    }
+
+
 })
 
-gallery.addEventListener('click', e=>{
+
+container.addEventListener('click', e=>{
     e.preventDefault();
 
     if(!e.target.closest('a')) return;
     
     const imgBigSrc = e.target.closest('a').getAttribute('href');
-    // console.log(imgBigSrc);
     const pop = document.createElement('aside');
     pop.classList.add('pop');
     let htmls = `
@@ -70,10 +90,10 @@ gallery.addEventListener('click', e=>{
                        <span class="btnClose">close</span> 
                     `
     pop.innerHTML = htmls;
-    gallery.append(pop);
+    gallery.after(pop);
 })
 
-gallery.addEventListener('click', e=>{
+container.addEventListener('click', e=>{
     const pop = document.querySelector('.pop');
     const btnClose = pop.querySelector('.btnClose');
 
