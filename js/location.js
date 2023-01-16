44070a80e3666ff9c3aa9a5241487909ba6dcf7c
aofur3957 <sunggin69@gmail.com>
2023-01-17 04:09:43 +0900

    location 페이지 스크립트 작업 완료

--- js/location.js
+++ js/location.js
@@ -1,22 +1,22 @@
-/*
-key : 9c03dc4ca7f0907cbe4777780120f513
-*/
 const store = document.querySelector('.store');
 const spots = store.querySelectorAll('article');
 
 const mapContainer = document.getElementById('map');
 const mapOptions = {
-    center: new kakao.maps.LatLng(37.5048632, 126.7633616),
-    level: 3,
+    center: new kakao.maps.LatLng(37.5048632, 126.7633616), // 지도 중심좌표
+    level: 3, // 지도 확대 레벨
 };
 
+// 지도를 담을 박스와 지도의 옵션이 담긴 객체를 인자로 전달하여 지도 생성
 const map = new kakao.maps.Map(mapContainer, mapOptions);
 
 const imgSrc = 'img/logo.png';
 const imgSize = new kakao.maps.Size(40, 40);
+// 이미지 주소와 사이즈를 인자로 전달하여 마커이미지 생성
 const markerImg = new kakao.maps.MarkerImage(imgSrc, imgSize);
 
-let positions = [
+// 여러개의 마커 정보가 담긴 배열
+const markerPositions = [
     {
         title: '부천점',
         latlng: new kakao.maps.LatLng(37.5048632, 126.7633616),
@@ -26,8 +26,8 @@ let positions = [
         latlng: new kakao.maps.LatLng(37.5614033, 126.9196564),
     },
     {
-        title: '건대점',
-        latlng: new kakao.maps.LatLng(37.5406289, 127.0631309),
+        title: '검단점',
+        latlng: new kakao.maps.LatLng(37.593968359668274, 126.71674448225654),
     },
     {
         title: '안양점',
@@ -35,32 +35,37 @@ let positions = [
     }
 ]
 
-for(let i=0;  i<positions.length; i++){
+// 위에서 만든 마커 정보가 담긴 배열을 모두 순회하면서 마커 생성
+for(let i=0;  i<markerPositions.length; i++){
     const marker = new kakao.maps.Marker({
         map: map,
-        position: positions[i].latlng,
-        title: positions[i].title,
+        position: markerPositions[i].latlng,
+        title: markerPositions[i].title,
         image: markerImg,
     })
 }
 
+// 각 spot에 해당하는 article을 클릭했을 때 해당 spot으로 지도 중심좌표 이동
 spots.forEach((spot, index)=>{
     spot.addEventListener('click', e=>{
-        let moveLatLon = positions[index].latlng;
+        const moveLatLon = markerPositions[index].latlng;
         map.setCenter(moveLatLon);
     })
 })
 
 setZoomable(false);
 
+// 지도 확대 축소(마우스 휠, 모바일 터치) 막기
 function setZoomable(zoomable){
     map.setZoomable(zoomable);
 }
 
+// 지도 내 컨트롤을 이용한 확대
 function zoomIn(){
     map.setLevel(map.getLevel() - 1);
 }
 
+// 지도 내 컨트롤을 이용한 축소
 function zoomOut(){
     map.setLevel(map.getLevel() + 1);
 }
