df2766cc1c88acbdbc2f5e572603d50a6738aee1
aofur3957 <sunggin69@gmail.com>
2023-01-16 21:13:06 +0900

    유튜브 페이지, 갤러리 페이지 스크립트 구현

--- js/gallery.js
+++ js/gallery.js
@@ -1,13 +1,3 @@
-/*
-api key: 6695bb82cf9a3db1962df3f386dd83e8
-Secret: 901a0ce8ce9b6b22
-
-base: https://www.flickr.com/services/rest/?
-
-method: flickr.interestingness.getList
-
-https://live.staticflickr.com/{server-id}/{id}_{secret}_{size-suffix}.jpg
-*/
 const container = document.querySelector('#gallery')
 const gallery = document.querySelector('.gallery');
 const loading = document.querySelector('.loading');
@@ -21,9 +11,9 @@ const api_key = '6695bb82cf9a3db1962df3f386dd83e8';
 const per_page = 10;
 const format = 'json';
 
-let url1 = `${base}method=${method}&api_key=${api_key}&per_page=${per_page}&format=${format}&nojsoncallback=1`;
+const url = `${base}method=${method}&api_key=${api_key}&per_page=${per_page}&format=${format}&nojsoncallback=1`;
 
-callData(url1);
+callPhoto(url);
 
 container.addEventListener('click', e=>{
     e.preventDefault();
@@ -33,7 +23,7 @@ container.addEventListener('click', e=>{
     const imgBigSrc = e.target.closest('a').getAttribute('href');
     const pop = document.createElement('aside');
     pop.classList.add('pop');
-    let htmls = `
+    const htmls = `
                        <img src=${imgBigSrc} alt="">
                        <span class="btnClose">close</span> 
                     `
@@ -43,62 +33,71 @@ container.addEventListener('click', e=>{
 
 container.addEventListener('click', e=>{
     const pop = document.querySelector('.pop');
-    const btnClose = pop.querySelector('.btnClose');
-
-    if(e.target === btnClose) pop.remove();
+    if(pop !== null){
+        const btnClose = pop.querySelector('.btnClose');
+        if(e.target === btnClose) pop.remove();
+    }
 })
 
 btnSearch.addEventListener('click', e=>{
     let tag = input.value;
     tag = tag.trim();
 
-    let url2 = `${base}method=${method2}&api_key=${api_key}&per_page=${per_page}&privacy_filter=1&format=${format}&nojsoncallback=1&tags=${tag}`;
+    const url = `${base}method=${method2}&api_key=${api_key}&per_page=${per_page}&privacy_filter=2&format=${format}&nojsoncallback=1&tags=${tag}`;
 
-    callData(url2);
+    callPhoto(url);
 })
 
-function callData(url){
+function callPhoto(url){
     fetch(url)
-    .then(data=>{
-        return data.json();
-    })
+    .then(data=>data.json())
     .then(json=>{
-        createList(json);
-        delayLoading();
+        getPhotoInfo(json);
+        // 이 위치에서 delayLoading 함수를 호출하면 아직 이미지가 비동기에 의해 로드되지 않은 상태이기 때문에 로딩 이미지가 보일 뿐 이미지가 로드된 화면은 보이지 않음
     })
 }
 
-function createList(json){
+function getPhotoInfo(json){
     let items = json.photos.photo;
 
     let htmls = '';
     items.map(item=>{
-        
-        let imgSrc = `https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_w.jpg`
-        let imgBigSrc = `https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_b.jpg`
-        
-        htmls +=  `
-                    <article>
-                        <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam, similique!</h1>
-                        <div class="wrap">
-                            <p>
-                                nsnr<br><br>
-                                Lorem ipsum dolor sit amet, consectetur 
-                            </p>
-                            <p>
-                                nsnr<br><br>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                            </p>
-                        </div>
-                        <a class="pic" href="${imgBigSrc}">
-                            <img src=${imgSrc} alt="">
-                        </a>
-                        <a href="${imgBigSrc}">view more</a>
-                    </article>
-                        `;
-
+        console.log(item);
+        fetch(`https://www.flickr.com/services/rest/?method=flickr.photos.getInfo&api_key=6695bb82cf9a3db1962df3f386dd83e8&photo_id=${item.id}&format=json&nojsoncallback=1`)
+        .then(data=>data.json())
+        .then(json=>{
+            htmls += createList(json);
+            gallery.innerHTML = htmls
+            delayLoading();
+        })       
     })
-    gallery.innerHTML = htmls;
+}
+
+function createList(json){
+    const photo = json.photo;
+            let imgSrc = `https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}_w.jpg`
+            let imgBigSrc = photo.originalsecret ? `https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.originalsecret}_4k.jpg` : `https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}_b.jpg`
+
+            return  `
+                        <article>
+                            <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam, similique!</h1>
+                            <div class="wrap">
+                                <p>
+                                    nsnr<br><br>
+                                    Lorem ipsum dolor sit amet, consectetur 
+                                </p>
+                                <p>
+                                    nsnr<br><br>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                </p>
+                            </div>
+                            <a class="pic" href="${imgBigSrc}">
+                                <img src=${imgSrc} alt="">
+                            </a>
+                            <a href="${imgBigSrc}">view more</a>
+                        </article>
+                    `;
+            
 }
 
 function delayLoading(){
@@ -116,3 +115,5 @@ function delayLoading(){
         }
     }
 }
+
+
--- js/youtube.js
+++ js/youtube.js
@@ -1,43 +1,103 @@
-/*
-api key = AIzaSyB2c-vJPxv0T0B9qWab28kZJ3_xr_57jhs
-request = https://www.googleapis.com/youtube/v3/playlistItems
-
-*/
 const key ='AIzaSyB2c-vJPxv0T0B9qWab28kZJ3_xr_57jhs';
 const part = 'snippet';
-const playlistId = [
-    'PL5zLxdZ1y87WGSvybcffoLpZvr4_XJg_z',
-    'PL5zLxdZ1y87VterXQCOaMjCwbf_KXj5Ve',
-    'PL5zLxdZ1y87V9XJZu_jxpF3wkRqoHDMeW'
-];
-const videos = document.querySelectorAll('#video');
+const channelId = 'UC_bAkCExw5ZgU1cTgrcZFTg';
+const videos = document.querySelectorAll('.video');
 const slogun = document.querySelector('.slogun');
+const sloguns = slogun.querySelectorAll('strong');
+console.log(sloguns);
+const delay = 2000;
 
-callData(playlistId[0], videos[0], 1);
-callData(playlistId[1], videos[1], 3);
-callData(playlistId[2], videos[2], 1);
+callPlayLists(channelId);
 
+// YouTube data api로 나의 채널로부터 모든 playlists 로드
+function callPlayLists(channelId){
+    const url = `https://www.googleapis.com/youtube/v3/playlists?part=${part}&key=${key}&channelId=${channelId}`;
+    
+    fetch(url)
+    .then(response=>response.json())
+    // youtube 메뉴에서 불러올 유튜브 목록만 필터링 후 순서 변경
+    .then(json=>{
+        const items = json.items;
+        const filteredItems = items.filter(item=>{
+            return item.snippet.title === 'The best house of the year' ? true : 
+                      item.snippet.title === 'Various Projects' ? true :
+                      item.snippet.title === '2 million hits on YouTube' ? true : false;
+        })
 
+        const deleted = filteredItems.splice(0, 1)[0];
+        filteredItems.splice(2, 0, deleted);
 
+        return filteredItems;
+    })
+    // 각 유튜브 목록을 순회하여 동영상 리소스를 로드
+    .then(filteredItems=>{
+        filteredItems.forEach((filteredItem, idx)=>{
+            callData(filteredItem.id, videos[idx]);
+        })
+        
+    })
+}
 
+// YouYube data api로 재생목록의 각 동영상 로드
+function callData(playlistId, container){
+    const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=${part}&key=${key}&playlistId=${playlistId}`;
+    
+    fetch(url)
+    .then(response=>response.json())
+    .then(json=>{
+        const items = json.items;
+        console.log(items);
+        items.forEach((item, idx)=>{
+            const articles = container.querySelectorAll('article');
+            let htmls;
+
+            if(container === videos[1]){
+                const tit = item.snippet.title
+                const des = item.snippet.description
+
+                htmls =  `
+                    <a class="pic" href="${item.snippet.resourceId.videoId}">
+                        <img src=${item.snippet.thumbnails.high.url} alt="">
+                    </a>
+                    <h4>${textSlice(tit, 30)}</h4>
+                    <p>${textSlice(des, 50)}</p>
+                `
+            }else{
+                htmls = `
+                    <a class="pic" href="${item.snippet.resourceId.videoId}">
+                        <img src=${item.snippet.thumbnails.high.url} alt="">
+                    </a>
+                `;
+            }
+            if(articles[idx]) articles[idx].insertAdjacentHTML('afterbegin', htmls);
+         });
+    });
+}
+
+// text가 길어질 경우 text를 length만큼 자르고 말줄임표(...)를 추가
+function textSlice(txt, length){
+    const sliced = txt.substring(0, length);
+    const ellipsis = [...sliced].concat('...').join('');
+    return ellipsis;
+}
 
 for(let video of videos){
     video.addEventListener('click', e=>{
+    // e.preventDefault 안하면 새로고침 됨 
     e.preventDefault();
 
-    let figure = document.createElement('figure');
-    if(!e.target.closest('img')) return;
-    let vidUrl = e.target.closest('.pic').getAttribute('href');
+    if(!e.target.closest('.pic')) return;
 
+    let vidUrl = e.target.closest('.pic').getAttribute('href');
+    const figure = document.createElement('figure');    
     figure.innerHTML = `
-    <iframe src="https://www.youtube.com/embed/${vidUrl}" width= "1000"  height= "600" frameborder = 0 allowfullscreen></iframe>
-    <button>close</button>
+        <iframe src="https://www.youtube.com/embed/${vidUrl}" width= "1000"  height= "600" frameborder="0" allowfullscreen></iframe>
+        <button>close</button>
     `
     video.append(figure);
-    })
+    });
 
     video.addEventListener('click', e=>{
-    
         let figure = document.querySelector('figure');
         if(figure !== null){
             let btnClose = figure.querySelector('button');
@@ -45,90 +105,35 @@ for(let video of videos){
         }
     })
 }
-   
 
-
-
-const delay = 2000;
+// 비동기로 동작하는 setInterval
 setInterval(()=>{
-    let sloguns = slogun.querySelectorAll('strong');
-    let arr_sloguns = Array.from(sloguns);
-    arr_sloguns.some((slogun, index)=>{
-        if(sloguns[0].classList.contains('on')){
-            for(let el of sloguns){
-                el.classList.remove('on');
-            }
-            sloguns[1].classList.add('on');
-            return true;
-        }else if(sloguns[1].classList.contains('on')){
-            for(let el of sloguns){
-                el.classList.remove('on');
-            }
-            sloguns[0].classList.add('on');
-            return true;
-        }
-    })
-}, delay)
+    for(let slogun of sloguns){
+        slogun.classList.contains('on') ? slogun.classList.remove('on') : slogun.classList.add('on');
+    }
+}, 2000)
 
-function callData(playlistId, container, num){
-    const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=${part}&key=${key}&playlistId=${playlistId}&maxResults=${num}`;
-    
-    fetch(url)
-    .then(response=>{
-    return response.json();
-    })
-    .then(json=>{
-        let items = json.items;
-        console.log(items);
 
-        if(num === 1){
-            let htmls = ' ';
-            items.forEach(item=>{
-            // let title = item.snippet.title;
-            // // console.log(item);
-            // if(title.length > 26){
-            //     title = title.slice(0, 26) + '...';
-            // }
-                htmls +=  `
-                <article>
-                    <a class="pic" href="${item.snippet.resourceId.videoId}">
-                        <img src=${item.snippet.thumbnails.high.url} alt="">
-                    </a>
-                    <div class="txt">
-                        <div class="container">
-                            <span>/ ARCHITECTURE</span>
-                            <h3>Lorem ipsum dolor sit amet</h3>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                        </div>
-                    </div>
-                </article>
-                                `
-                container.innerHTML = htmls;
-            })
-        }else if(num === 3){
-            let htmls = `
-            <div class="txt">
-                <span>ARCHITECTURE</span>
-                <h3>Lorem ipsum dolor sit amet consectetur,</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem.</p>
-            </div>
-                            `;
-            items.map(item=>{
-                htmls += `
-                <article>
-                    <a class="pic" href=${item.snippet.resourceId.videoId}>
-                        <img src=${item.snippet.thumbnails.high.url} alt="">
-                    </a>
-                    <h4>Lorem, ipsum dolor.</h4>
-                    <p>Lorem ipsum dolor sit amet consectetur setrual.</p>
-                </article>
-                                `
-                container.innerHTML = htmls;
-            })
-        }
-         
-    })
-} 
+// setInterval(()=>{
+//     let sloguns = slogun.querySelectorAll('strong');
+//     let arr_sloguns = Array.from(sloguns);
+//     arr_sloguns.some((slogun, index)=>{
+//         if(sloguns[0].classList.contains('on')){
+//             for(let el of sloguns){
+//                 el.classList.remove('on');
+//             }
+//             sloguns[1].classList.add('on');
+//             return true;
+//         }else if(sloguns[1].classList.contains('on')){
+//             for(let el of sloguns){
+//                 el.classList.remove('on');
+//             }
+//             sloguns[0].classList.add('on');
+//             return true;
+//         }
+//     })
+// }, delay)
+
 
 
 
